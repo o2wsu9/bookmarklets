8c40579a12817ec2e792a887b3cc7e7485f6af14
o2wsu9 <o2wsu9@gmail.com>
2016-07-15 11:24:11 +0900

    Update get_ss_key.js

--- get_ss_key.js
+++ get_ss_key.js
@@ -1,6 +1,1 @@
-(function(){
-  var i = location.href;
-  i = i.replace(/.*\/spreadsheets\/d\//, "");
-  i = i.replace(/\/.*/,"");
-  prompt("Spreadsheet key", i);
-})()
+javascript:(function(){var i=location.href;i=i.replace(/.*\/spreadsheets\/d\//,"");i=i.replace(/\/.*/,"");prompt("Spreadsheet key",i);})()
